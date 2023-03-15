3010d27c8621409c4163b7f2457fd6c899249b52
Aleksandr Volkov <ramwoolf@gmail.com>
2023-03-15 22:46:46 +0100

    Fix the unsigned type effects, encoding works

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,28 @@
 
 //using uint8 = unsigned char;
 
-uint8_t const CHAR_TABLE[64] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+char const CHAR_TABLE[64] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                               'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
-uint8_t const filler_char = '=';
+char const filler_char = '=';
 
 void encode_base64(uint8_t const *original_str, uint8_t* encoded_str) {
     assert(original_str != nullptr);
     assert(encoded_str != nullptr);
-    uint8_t const ascii_byte_size = 8u;
-    uint8_t const base64_char_size = 6u;
+    unsigned int constexpr ascii_byte_size = 8u;
+    unsigned int constexpr base64_char_size = 6u;
 
-    unsigned int origin_str_index = 0;
+    unsigned int origin_str_index = 0u;
     unsigned int encoded_str_index = 0u;
-    uint8_t tail_size = 0u;
-    uint8_t bit_sextet_prefix = 0u;
-    uint8_t char_table_index = 0u;
+    unsigned int tail_size = 0u;
+
+    unsigned int bit_sextet_prefix = 0u;
+    unsigned int char_table_index = 0u;
 
     while (original_str[origin_str_index] != '\0') {
         tail_size += ascii_byte_size - base64_char_size;
-        auto prefix = bit_sextet_prefix << (ascii_byte_size - tail_size);
-        auto postfix = original_str[origin_str_index] >> tail_size;
+        char prefix = bit_sextet_prefix << (ascii_byte_size - tail_size);
+        char postfix = original_str[origin_str_index] >> tail_size;
         char_table_index = prefix + postfix;
         bit_sextet_prefix = original_str[origin_str_index] & ((1u << tail_size) - 1u);
         encoded_str[encoded_str_index] = CHAR_TABLE[char_table_index];
@@ -37,9 +38,9 @@ void encode_base64(uint8_t const *original_str, uint8_t* encoded_str) {
             bit_sextet_prefix = 0u;
         }
     }
-    if (tail_size != 0u)
+    if (tail_size != 0)
     {
-        uint8_t last_coded_char_index = original_str[origin_str_index - 1u] & ((1u << tail_size) - 1u) << (base64_char_size - tail_size);
+        unsigned int last_coded_char_index = original_str[origin_str_index - 1u] & ((1u << tail_size) - 1u) << (base64_char_size - tail_size);
         encoded_str[encoded_str_index] = CHAR_TABLE[char_table_index];
         do
         {
@@ -53,17 +54,21 @@ void encode_base64(uint8_t const *original_str, uint8_t* encoded_str) {
     encoded_str[encoded_str_index] = '\0';
 }
 
-void decode_base64(uint8_t const *encoded_str, uint8_t* original_str) {
+void decode_base64(uint8_t const *encoded_str, uint8_t* restored_str) {
     assert(encoded_str != nullptr);
-    assert(original_str != nullptr);
+    assert(restored_str != nullptr);
+
 }
 
 int main(int argc, char**argv)
 {
-    uint8_t original_str[] {"ManMa"};
-    uint8_t encoded_str[10] {'\0'};
+    uint8_t original_str[] {"Many hands make light work."};
+    uint8_t encoded_str[100] {'\0'};
+    uint8_t restored_str[100] {'\0'};
     encode_base64(original_str, encoded_str);
     std::cout << encoded_str << std::endl;
+    decode_base64(encoded_str, restored_str);
+    std::cout << restored_str << std::endl;
 
     return 0;
 }
